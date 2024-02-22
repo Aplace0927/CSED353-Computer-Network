d1abf981239e178a861071266f9967ebbda7912c
Aplace <aplace030927@gmail.com>
2024-02-22 03:39:30 -0800

    feat(bytestream): reader-side implementation

--- libsponge/byte_stream.cc
+++ libsponge/byte_stream.cc
@@ -1,5 +1,6 @@
 #include "byte_stream.hh"
 
+#include <iostream>
 // Dummy implementation of a flow-controlled in-memory byte stream.
 
 // For Lab 0, please replace with a real implementation that passes the
@@ -12,20 +13,12 @@ void DUMMY_CODE(Targs &&.../* unused */) {}
 
 using namespace std;
 
-ByteStream::ByteStream(const size_t capacity) {
-    _is_writing = false;
-    _buffer = std::string("");
-    _capacity = capacity;
-    _remaining_capacity = 0U;
-    _bytes_written = 0U;
-    _bytes_read = 0U;
-}
+ByteStream::ByteStream(const size_t capacity) : _capacity(capacity), _remaining_capacity(capacity) {}
 
 size_t ByteStream::write(const string &data) {
-    int write_length = std::min<std::size_t>(data.length(), _remaining_capacity);
+    const size_t write_length = std::min<std::size_t>(data.length(), remaining_capacity());
 
     try {
-        _is_writing = true;
         _buffer.append(data.substr(0U, write_length));
     } catch (const std::exception &e) {
         return update_write(0U);
@@ -35,19 +28,46 @@ size_t ByteStream::write(const string &data) {
 
 //! \param[in] len bytes will be copied from the output side of the buffer
 string ByteStream::peek_output(const size_t len) const {
-    DUMMY_CODE(len);
-    return {};
+    const size_t peek_length = std::min<std::size_t>(len, buffer_size());
+
+    try {
+        return _buffer.substr(0U, peek_length);
+    } catch (const std::exception &e) {
+        return std::string("");
+    }
 }
 
 //! \param[in] len bytes will be removed from the output side of the buffer
-void ByteStream::pop_output(const size_t len) { DUMMY_CODE(len); }
+void ByteStream::pop_output(const size_t len) {
+    const size_t read_length = std::min<std::size_t>(len, buffer_size());
+
+    try {
+        _buffer.erase(0, read_length);
+    } catch (const std::exception &e) {
+        update_read(0U);
+        return;
+    }
+    update_read(read_length);
+    return;
+}
 
 //! Read (i.e., copy and then pop) the next "len" bytes of the stream
 //! \param[in] len bytes will be popped and returned
 //! \returns a string
 std::string ByteStream::read(const size_t len) {
-    DUMMY_CODE(len);
-    return {};
+    const size_t read_length = std::min<std::size_t>(len, buffer_size());
+
+    std::string read_data;
+
+    try {
+        read_data = _buffer.substr(0, read_length);
+        _buffer.erase(0, read_length);
+    } catch (const std::exception &e) {
+        update_read(0U);
+        return std::string("");
+    }
+    update_read(read_length);
+    return read_data;
 }
 
 void ByteStream::end_input() {
@@ -55,30 +75,30 @@ void ByteStream::end_input() {
     return;
 }
 
-bool ByteStream::input_ended() const { return {}; }
+bool ByteStream::input_ended() const { return _is_writing == false; }
 
-size_t ByteStream::buffer_size() const { return _capacity; }
+size_t ByteStream::buffer_size() const { return _capacity - _remaining_capacity; }
 
 bool ByteStream::buffer_empty() const { return _capacity == _remaining_capacity; }
 
-bool ByteStream::eof() const { return false; }
+bool ByteStream::eof() const { return input_ended() && buffer_empty(); }
 
-size_t ByteStream::bytes_written() const { return {}; }
+size_t ByteStream::bytes_written() const { return _bytes_written; }
 
-size_t ByteStream::bytes_read() const { return {}; }
+size_t ByteStream::bytes_read() const { return _bytes_read; }
 
 size_t ByteStream::remaining_capacity() const { return _remaining_capacity; }
 
 size_t ByteStream::update_read(size_t length_read) {
     _bytes_read += length_read;
-    _remaining_capacity -= length_read;
+    _remaining_capacity += length_read;
 
     return length_read;
 }
 
 size_t ByteStream::update_write(size_t length_written) {
     _bytes_written += length_written;
-    _remaining_capacity += length_written;
+    _remaining_capacity -= length_written;
 
     return length_written;
 }
