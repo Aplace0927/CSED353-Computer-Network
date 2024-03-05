6cee0f4fd3867c418c07d7eb0505dd9d0d623b2a
Aplace <aplace030927@gmail.com>
2024-03-05 09:23:37 -0800

    base(reassembler): write baseline (pseudo) codes

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -12,13 +12,62 @@ void DUMMY_CODE(Targs &&.../* unused */) {}
 
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
+StreamReassembler::StreamReassembler(const size_t capacity)
+    : _output(capacity), _capacity(capacity), _datagram_arrived() {}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    /* Ignore far-future packet (that cannot be stored on buffer)*/
+    if (index >= _unread_until + _capacity) {
+        return;
+    }
+
+    /* EOF packet found*/
+    if (eof) {
+        _eof_at = index + data.length();
+    }
+
+    /* Trim out unacceptable / already read substrings - for extreme cases*/
+    Datagram curr(index, data);
+    if (curr.from() < _unread_until + _capacity and _unread_until + _capacity <= curr.to()) {
+        curr._data = curr._data.substr(0, max(0UL, _unread_until + _capacity - curr.from()));
+    }
+    if (curr.from() < _unread_until and _unread_until <= curr.to()) {
+        curr._data = curr._data.substr(_unread_until - curr.from());
+        curr._from = _unread_until;
+    }
+
+    /**
+     * TODO: Stitch Bytes
+     *
+     * If target has intersection with each `Datagram`s:
+     *  - Covered by / Covers with / Left intersect / Right intersect
+     *
+     * THIS SEQUENCE MIGHT HAPPEN CONSEQUENTLY.
+     *
+     * Covered by: then ignore current datagram (already covered)
+     * Covers with: remove covered datagram, push current.
+     * Left-intr / Right intr:
+     *      set its index to left one's index,
+     *      set data as union of both datagrams
+     */
+
+    while (_datagram_arrived.empty() == false and _datagram_arrived.begin()->from() == _unread_until) {
+        /**
+         * FIX: Fix not compatible error on `stream_reassembler.cc:61`
+         */
+        _unread_until += _datagram_arrived.begin()->len();
+
+        _output.write(_datagram_arrived.begin()->_data);
+        _datagram_arrived.erase(_datagram_arrived.begin());
+    }
+
+    /* Assemble until EOF found */
+    if (_eof_at == _unread_until and _eof_at != 0) {
+        _output.end_input();
+    }
 }
 
 size_t StreamReassembler::unassembled_bytes() const { return {}; }
