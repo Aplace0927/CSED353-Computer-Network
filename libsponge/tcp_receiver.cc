8776f44d81612c3bf294eadb421dfc3396af73d9
Aplace <aplace030927@gmail.com>
2024-03-15 18:28:36 -0700

    feat(tcprecv): implement \`segment_recieved\`

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -1,17 +1,32 @@
 #include "tcp_receiver.hh"
 
+#include <iostream>
 // Dummy implementation of a TCP receiver
 
 // For Lab 2, please replace with a real implementation that passes the
 // automated checks run by `make check_lab2`.
 
 template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+void DUMMY_CODE(Targs &&.../* unused */) {}
 
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    _syn |= seg.header().syn;
+    if (not _syn) {
+        return;
+    }
+    if (seg.header().syn) {
+        _isn = seg.header().seqno;
+    }
+
+    _chkpoint += seg.length_in_sequence_space();
+
+    uint64_t stream_idx =
+        unwrap(WrappingInt32(seg.header().seqno + static_cast<uint32_t>(seg.header().syn)), _isn, _chkpoint) - 1;
+
+    _reassembler.push_substring(seg.payload().copy(), stream_idx, seg.header().fin);
+    return;
 }
 
 optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
--- libsponge/tcp_receiver.hh
+++ libsponge/tcp_receiver.hh
@@ -20,6 +20,10 @@ class TCPReceiver {
     //! The maximum number of bytes we'll store.
     size_t _capacity;
 
+    bool _syn = false;
+    WrappingInt32 _isn = WrappingInt32(0);
+    uint64_t _chkpoint = 0ULL;
+
   public:
     //! \brief Construct a TCP receiver
     //!
